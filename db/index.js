02080a388a2d38880c8d33fb4d9afb56317b1aa9
Tim Quigg <timjquigg@gmail.com>
2022-12-30 15:38:03 -0700

    Update db connection info for Heroku Postgres

--- db/index.js
+++ db/index.js
@@ -2,14 +2,20 @@ import { Pool } from "pg";
 
 let pg;
 
+const dbParams = {
+  user: process.env.PGSQL_USER,
+  password: process.env.PGSQL_PASSWORD,
+  host: process.env.PGSQL_HOST,
+  port: process.env.PGSQL_PORT,
+  database: process.env.PGSQL_DATABASE,
+};
+
+if (process.env.NODE_ENV === "production") {
+  dbParams["ssl"] = { rejectUnauthorized: false };
+}
+
 if (!pg) {
-  pg = new Pool({
-    user: process.env.PGSQL_USER,
-    password: process.env.PGSQL_PASSWORD,
-    host: process.env.PGSQL_HOST,
-    port: process.env.PGSQL_PORT,
-    database: process.env.PGSQL_DATABASE,
-  });
+  pg = new Pool(dbParams);
 }
 
 export default pg;
